b333c9edf8c03b11d3cb00322ce9ab22345dc4e5
Bruno Korbar <bjuncek@gmail.com>
2017-10-04 20:35:13 -0400

    initial PCB implementation

---
+++ src/pcb.h
@@ -0,0 +1,33 @@
+#ifndef _PCB_H_
+#define _PCB_H_
+#include "lock.h"
+#include "list.h"
+// #include wherever User and kernel context are
+
+typedef struct _PCB { 
+
+	/* Process tracking */
+	
+	// should we make it unsigned? 
+	int process_id;
+	//User Context *user_context;
+	//Kernel Context *kernel_context;
+	lock *process_lock;
+
+	/* Hierarchy structure */
+	// if parent
+	node *children;
+	node *zombiez;
+
+	// if child
+	struct _PCB *parent; //we need to know new 
+	int exit_status;
+
+	/* Process Memory management */
+
+	int brk_address;
+	int neap_start_pg; //or void *heap_start
+	
+
+
+#endif // _PCB_H_
