07593be81aee57d9032740083958b062fe5073b1
Bruno Korbar <bjuncek@gmail.com>
2017-10-04 20:38:31 -0400

    Added list include

--- src/lock.h
+++ src/lock.h
@@ -1,11 +1,13 @@
-/*
- */
+#ifndef _LOCK_H_
+#define _LOCK_H_
 typedef struct _LOCK
 {
         int id; // id of lock
         int claimed; // 0 for free, 1 for claimed
         unsigned int proc_id; //who has the lock.
-	// question: shouldn't it just be Node * waiters? 
-        struct _Node *waiters; //who has asked for the lock and will get it next
+        struct node *waiters; //who has asked for the lock and will get it next
 } __LOCK;
 typedef struct _LOCK lock;
+
+
+#endif
