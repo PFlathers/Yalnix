e535f57f9409ce82550c49e0c95a0b0d4a3caa21
Bruno Korbar <bjuncek@gmail.com>
2017-10-04 21:03:44 -0400

    added pipe support, just for check1

---
+++ src/pipe.h
@@ -0,0 +1,16 @@
+#ifndef _PIPE_H_
+#define _PIPE_H_
+
+typedef struct _PIPE {
+  int id;
+  int length;
+  char *buffer;
+
+
+  // probably something else, we should
+  // erad the textbook
+  
+} _PIPE;
+
+
+#endif
