337c30c45d3199630499d6feb1c7e3cfacde1c07
Bruno Korbar <bjuncek@gmail.com>
2017-10-04 20:41:51 -0400

    Cleaned up and included LL

--- src/cvar.h
+++ src/cvar.h
@@ -1,9 +1,18 @@
-/*
- */
+#ifndef _CVAR_H_
+#define _CVAR_H_
+
+#include "list.h"
+
+
 typedef struct _CVAR
 {
         int id; //id of the cvar
         int claimed; // 0 if not claimed. 1 if claimed
-        struct _Node *waiters; // who is waiting on signal.
-} __CVAR;
+        struct node *waiters; // who is waiting on signal.
+} _CVAR;
 typedef struct _CVAR CVAR;
+
+/* Public Facing Function Calls */
+
+
+#endif
