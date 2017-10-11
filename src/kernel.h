3e9778fc34bfa09a56dc4dda4f457054819bce16
Bruno Korbar <bjuncek@gmail.com>
2017-10-11 09:56:37 -0400

    addressing sean 's comments: kernel.h

---
+++ src/kernel.h
@@ -0,0 +1,34 @@
+#ifndef _KERNEL_H_
+#define _KERNEL_H_
+
+/*
+ * System Includes From Sean's Manual
+ */
+#include <hardware.h>
+#include <yalnix.h>
+
+
+/*
+ * Local Includes
+ */
+#include "listlist.h"
+#include "pcb.h"
+
+
+// globals
+int next_resource_id;
+list *locks;
+list *cvars;
+list *pipes;
+list *ttys;
+
+
+
+
+
+
+
+/* Public Facing Function Calls */
+
+
+#endif
