14f4115fa53e0f2d4e58f587166d8548d6c36c2b
Bruno Korbar <bjuncek@gmail.com>
2017-10-12 14:06:47 -0400

    added test readme and h-file

--- test/utest_list.c
+++ test/utest_list.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "test.h"
 #include "../src/globals.h"
 #include "../src/list.h"
 
@@ -8,7 +9,7 @@
 /* runs the unit test for list interface */
 int main ( int argc, char **argv ) 
 {
-	TracePrintf(1, "UTEST: list --> init \n");
+	TracePrintf(TEST_TRACE, "UTEST: list --> init \n");
 
 	// init list mallocs it for us
 	list *test_list = init_list();
@@ -17,7 +18,7 @@ int main ( int argc, char **argv )
 	}
 
 
-	TracePrintf(1, "UTEST: list --> add and print \n");
+	TracePrintf(TEST_TRACE, "UTEST: list --> add and print \n");
 	for (int i=0; i<4; i++){
 		int data = i;
 		list_add(test_list, (void *) &data);
@@ -29,13 +30,13 @@ int main ( int argc, char **argv )
 	}
 
 	
-	TracePrintf(1, "UTEST: list --> remove \n");
+	TracePrintf(TEST_TRACE, "UTEST: list --> remove \n");
 	for (int i=0; i<4; i++) {
 		list_remove( test_list, i );
 	}
 
 
-	TracePrintf(1, "UTEST: list --> success \n");
+	TracePrintf(TEST_TRACE, "UTEST: list --> success \n");
 
 	free(test_list);
 	return SUCCESS;
