f0b48a0aff84b6779c58c920ee516ce800021fec
Patrick Flathers <pflathers@cs.dartmouth.edu>
2017-10-15 14:01:00 -0400

    more prototyping.

---
+++ src/tty.c
@@ -0,0 +1,14 @@
+#include "global.h"
+#include "list.h"
+#include "tty.h"
+
+
+int TtyRead(int tty_id, void *buf, int len)
+{
+	return 0;
+}
+
+int TtyWrite(int tty_id, void *buf, int len)
+{
+	return 0;
+}
