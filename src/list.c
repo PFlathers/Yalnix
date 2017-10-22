1d731e7a2c8cf663de08c6b1af02298e428d52fe
Bruno Korbar <bjuncek@gmail.com>
2017-10-22 14:59:29 -0400

    Changed list pop to return data

--- src/list.c
+++ src/list.c
@@ -88,11 +88,11 @@ int list_remove(List *list_to_remove, void *data)
 void *list_pop(List *list_to_pop)
 {
 	Node *pop_node = list_to_pop->head;
-	//void *ret_data = pop_node->data;
+	void *ret_data = pop_node->data;
 	list_to_pop->head = list_to_pop->head->next;
 	list_to_pop->head->prev = NULL;
-	//free(pop_node);
-	return pop_node;
+	free(pop_node);
+	return ret_data;
 }
 
 int list_count(List *list_to_count)
