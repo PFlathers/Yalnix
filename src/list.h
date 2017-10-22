99fffd584f01e697959d9103b4ae66ca212f4ff2
Bruno Korbar <bjuncek@gmail.com>
2017-10-22 14:58:34 -0400

    Removed unnecesary list items in list.h

--- src/list.h
+++ src/list.h
@@ -5,7 +5,6 @@ typedef struct _Node
 {
 	struct _Node *next;
 	struct _Node *prev;
-	int id;
 	void *data;
 } _Node;
 typedef struct _Node Node;
@@ -20,7 +19,6 @@ typedef struct _List List;
 /* Public Facing Function Calls */
 int list_remove(List *list_to_remove, void *data);
 void list_add(List *list_to_add, void *data);
-void list_add2(List *list_to_add, void *data, int id);
 List *init_list();
 void *list_pop(List *list_to_pop);
 int list_count(List *list_to_count);
