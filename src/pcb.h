9f4e6d8af3bb80c8640c7840ebd31ee4f675f677
Bruno Korbar <bjuncek@gmail.com>
2017-10-22 06:31:41 -0400

    created delay handler datastructure and included it to the pcb

--- src/pcb.h
+++ src/pcb.h
@@ -6,7 +6,37 @@
 #include "lock.h"
 #include "list.h"
 
-// #include wherever User and kernel context are
+
+
+
+
+/*
+linux has blocked signal hanler
+dtype, 
+this is a fake attempt to copy that
+https://web.cs.wpi.edu/~claypool/courses/3013-A03/samples/linux-pcb.c
+*/
+
+#define INACTIVE    ((u_long) 0x0)
+#define ACTIVE      ((u_long) 0x1)
+
+#define NO_BLOCK ((u_long) 0x0)
+#define DELAY ((u_long) 0x1)
+
+typedef struct _DELAY {
+	// we are either blocking or not
+	union{
+		int count; // count of the deyay
+		int ret; // return value of the blocking party
+	}
+
+	u_long is_active; // check if active
+	u_long type; // check what kind for sys handler
+
+	void *data;
+} _DELAY;
+
+typedef struct _DELAY DelayHandler;
 
 typedef struct _PCB { 
 
@@ -16,9 +46,10 @@ typedef struct _PCB {
 	UserContext *user_context;
 	KernelContext *kernel_context;
 
-
-	unsigned int clock_ticks;
-	unsigned int timeflag = 0;
+	// see above
+	DelayHandler block;
+	// unsigned int clock_ticks;
+	// unsigned int timeflag = 0;
 	//lock *process_lock;
 
 	/* Hierarchy structure */
