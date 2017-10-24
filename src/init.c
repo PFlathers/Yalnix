51eb3dbc270bbef807012d285f0e9396f8013764
Bruno Korbar <bjuncek@gmail.com>
2017-10-23 22:55:00 -0400

    thorough cleanup

--- src/init.c
+++
@@ -1,9 +0,0 @@
-#include <yalnix.h>
-/* moddeled after DoIdle */
-int main(int argc, char *argv[]) {
-    while (1) {
-        TracePrintf(1, "Do Init Prog!\n");
-        Pause();
-    }
-    return 0;
-}
--- src/loadprog.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LOADPROG_H_
-#define _LOADPROG_H_
-
-int LoadProgram(char *name, char *args[], pcb *proc);
-
-#endif
