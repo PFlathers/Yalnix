7371317a2477bec3bf805b2f0543dc7d36f90af1
Bruno Korbar <bjuncek@gmail.com>
2017-10-29 23:48:24 -0400

    added fork testfile

---
+++ userland/fork.c
@@ -0,0 +1,25 @@
+#include <yalnix.h>
+
+int main()
+{
+	int i = Fork();
+        char *args[] = {"init", 0};
+        TracePrintf("%d\n", i);
+	if(i == 0){
+		while(1){
+                TracePrintf(0, "I is child");
+                Exec("init", args);
+                Pause();
+                }
+	}
+        else{
+                while(1){
+                        Pause();
+                }
+        }
+
+
+	TracePrintf(0, "I is parent");
+	
+	return 0;
+}
