ce89ddea10819d4bda4e98292555721c65c16714
Bruno Korbar <bjuncek@gmail.com>
2017-11-04 16:28:16 -0400

    tty Write: find buffer to write

--- src/tty.c
+++ src/tty.c
@@ -26,7 +26,31 @@ int TtyRead(int tty_id, void *buf, int len)
 
 int TtyWrite(int tty_id, void *buf, int len)
 {
+        TracePrintf(3, "TtyWrite ### Start\n");
+
 	// look up tty by it's id
+        TTY *tty = NULL;
+        Node * node =  ttys->head;
+        while (node->next != NULL){
+                if (node->data->id == tty_id){
+                        tty = (TTY *) node->data;
+                        break;
+                }
+                node = node->next;
+        }
+        //edge case
+        if (node->next->data->id == tty_id) {
+                tty = tty->next->data;
+        }
+
+        if (tty == NULL) {
+                TracePrintf(6, "TtyWrite: ERROR; tty %d out of bounds \
+                                - should not happen \n", tty_id);
+                return ERROR;
+        }
+
+
+
 
         // allocate new write buffer in the current process
         // copy buffer in it
@@ -41,6 +65,6 @@ int TtyWrite(int tty_id, void *buf, int len)
         // move on
         goto_next_process(curr_proc->user_context, 0);
 
-
+        TracePrintf(3, "TtyWrite ### End, returning %d\n", len);
         return len;
 }
