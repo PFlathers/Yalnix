7365315c7bb44ac2940d87034d7f0d6ee225b71a
Patrick Flathers <pflathers@cs.dartmouth.edu>
2017-11-12 23:16:46 -0500

    tested cvar

---
+++ userland/cvar_b.c
@@ -0,0 +1,50 @@
+#include <yalnix.h>
+#include <string.h>
+
+int main()
+{
+
+        
+        char a [10];
+        char b [10];
+        strcpy(a, "init");
+        strcpy(b, "\0");
+
+        char *args[] = {a,b};
+        //TracePrintf("%d\n", i);
+        
+	int i = Fork();
+        int j, k;
+        int retval = LockInit(&j); 
+        int retval2 = CvarInit(&k);
+
+        //Acquire(i);
+        
+	if(i == 0){
+                while (Acquire(j)  != 0);
+                CvarWait(k, j);
+
+                /*
+		while(1){
+                TracePrintf(0, "I is child");
+                Exec("init", args);
+                Pause();
+                }
+*/
+		return 0;
+	}
+        else{
+/*
+                while(1){
+                        Pause();
+                }
+*/
+//                Wait(&i);
+                CvarBroadcast(k);
+        }
+
+
+	//TracePrintf(0, "I is parent");
+	
+	return 0;
+}
