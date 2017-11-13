3e73da195f02d436fb05c80d2463326fd1262c59
Patrick Flathers <pflathers@cs.dartmouth.edu>
2017-11-13 14:57:37 -0500

    documentation++ for include/kernel_utils.h

--- include/kernel_utils.h
+++ include/kernel_utils.h
@@ -4,12 +4,18 @@
 #include "pipe.h"
 #include "lock.h"
 
+//finds and returns cvar from passed id
 Cvar *kernel_findCvar(int cvar_id);
 
+//finds and returns lock from passed id
 Lock *kernel_findLock(int lock_id);
 
+//finds and returns pipe from passed id
 Pipe *kernel_findPipe(int lock_id);
 
+/* WARNING!!! THIS IS A DANGEROUS FUNCTION!
+ * trashes, frees, and recycles the page tables from the passed process.
+ */
 void free_pagetables(pcb* myproc);
 
 #endif
