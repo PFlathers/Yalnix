4580b0bb6fa22c9c2dc2e7ec953298c8558d55e2
Patrick Flathers <pflathers@cs.dartmouth.edu>
2017-11-13 20:16:18 -0500

    documentation for list.h

--- include/list.h
+++ include/list.h
@@ -18,11 +18,25 @@ typedef struct _List List;
 
 
 /* Public Facing Function Calls */
-int list_remove(List *list_to_remove, void *data);
-void list_add(List *list_to_add, void *data);
+
+//creates a list
 List *init_list();
+
+// adds data onto the end of the list
+void list_add(List *list_to_add, void *data);
+
+// add data onto the begining of the list
 void list_push(List *list_to_push, void *data);
+
+// removes and returns data from the begining of the list
 void *list_pop(List *list_to_pop);
+
+// returns the size of the list
 int list_count(List *list_to_count);
+
+// removes the data from the list
+int list_remove(List *list_to_remove, void *data);
+
+// removes and frees data form the list
 int list_remove_non_delete(List *list_to_remove, void *data);
 #endif
